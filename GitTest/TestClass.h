e038cd84eb1fb0142ee1d544b248f9627234ce16
Alena <awilar@mail.ru>
2015-01-09 00:20:44 +0300

    create new class and new branch

---
+++ GitTest/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  GitTest
+//
+//  Created by Alena on 09.01.15.
+//  Copyright (c) 2015 Alena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
