392a644fb530acb4482434091eb1dbbd644f95fa
Alena <awilar@mail.ru>
2015-01-09 00:02:18 +0300

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Alena on 09.01.15.
+//  Copyright (c) 2015 Alena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Alena on 09.01.15.
+//  Copyright (c) 2015 Alena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
